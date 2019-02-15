63fcd1b178b38467d0ebc2a4ca917446647e81b7
R-Cole <randy@goodglue.com>
2019-02-15 14:30:36 -0700

    adjusted array

--- src/components/gameRules.js
+++ src/components/gameRules.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import GameRulesList from './gameRulesList';
 
 export class GameRules extends React.Component {
     componentDidMount() {
     }    
 
     render() {
+
+    let testSelectedGame = {
+            rules:[{description:'this is a rule...'},{description:'this is a another rule...'},{description:'this is yet another rule...'},{description:'this is, yes, another awesome rule...'}],
+          };  
+
       return (
         <div className="gameRules">
-      
+        <h3>{this.props.selectedGame.name}</h3>
+        <h3>Game Rules!</h3>
+        <GameRulesList selectedGameRules={this.props.selectedGame.rules}/>
         </div>
         );
     }
@@ -16,6 +24,7 @@ export class GameRules extends React.Component {
 
 const mapStateToProps = state => {
     return {
+        selectedGame:state.game.data
     };
 };
 
