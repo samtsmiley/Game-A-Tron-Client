68413225299b9062c4bcfd2d9cd56a82740649c4
R-Cole <randy@goodglue.com>
2019-02-19 15:16:39 -0700

    reading state into leaderBoard

--- src/components/gameLeaderboard.js
+++ src/components/gameLeaderboard.js
@@ -2,26 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import GameLeaderboardListPlayers from './gameLeaderboardListPlayers';
 import './gameLeaderboard.css'
-
-//this needs to the game based on id from state:
-//then participants
-//     {
-//        userId:
-//        score:
-//                  }
-//then find user name based on userId...
-//OR
-//...if endpoint provides the name then that way will be better...
-
+ 
 export class GameLeaderboard extends React.Component {
     componentDidMount() {
     }    
 
     render() {
-
-    if(this.props.selectedGame){console.log('GAME DATA: ',this.props.selectedGame.participants);}
-    //console.log('username? ',this.props.selectedGame.participants[0].userId.username); 
-
+ 
   console.log('score? ',this.props.selectedGame.participants[0].score);
 
       return (
