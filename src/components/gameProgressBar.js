8fed953fc57a92f255e7da37506b2bab2f263a79
R-Cole <randy@goodglue.com>
2019-02-20 12:16:41 -0700

    progressbar working

--- src/components/gameProgressBar.js
+++ src/components/gameProgressBar.js
@@ -2,23 +2,37 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { Line, Circle } from 'rc-progress';
 
+//require --> npm install --save rc-progress
+
 export class GameProgressBar extends React.Component {
     componentDidMount() {
     }    
 
     render() {
+        
+        let percentProgress = 0;
+        let currentScore = this.props.score;
+        let maxScore = this.props.endScore;
 
-        let percentProgress = 55;
+        if(currentScore && maxScore){
+
+            percentProgress = ((currentScore/maxScore) * 100).toFixed(2);
+
+        }
+
+        
+ 
+        //}
 
         let barContainerStyle = {
             width: '70%',
             height: '70%',
             display: 'inline-block',
-          };
+        };
 
       return (
         <div >
-            <h3>Your progress so far {percentProgress}%: </h3>
+            <h3>Your progress so far is {percentProgress}%: </h3>
             <div style={barContainerStyle}>
             <Line percent={percentProgress} strokeWidth="4" trailWidth="3.75" trailColor="#bebec8" strokeColor="#4aa84a" strokeLinecap='round'/>
             {/* <Circle percent="0" strokeWidth="4" trailWidth="3.75" trailColor="#beb4b4" strokeColor="#4aa84a" strokeLinecap='round'/> */}
@@ -29,9 +43,24 @@ export class GameProgressBar extends React.Component {
 }
 
 const mapStateToProps = state => {
+    
+    let score = 0;
+    if(state.game.participants){
+        if(state.game.participants.length === 0){score = 0}else{
+            score = state.game.data.participants.find(participant => participant.userId.id === state.auth.currentUser.id).score
+        }
+    }
+ 
+    
+    let endScore = 0;
+    if(state.game.data.endScore){endScore = 0}else{
+        endScore = state.game.data.endScore
+    }
     return {
-        gameParticipants: state.game.data.participants
+        score,
+        endScore 
     };
+    
 };
 
 export default connect(mapStateToProps)(GameProgressBar);
