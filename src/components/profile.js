49a5f45ede97ba9fef9d096fe8672ff61fbae9cd
R-Cole <randy@goodglue.com>
2019-02-23 12:44:03 -0700

    posts with images working

--- src/components/profile.js
+++ src/components/profile.js
@@ -12,10 +12,12 @@ export class Profile extends React.Component {
   render() {
     const posts = this.props.myPosts.map(post =>
       <li className="onePost subcard" key={post.id}>    
-      <p className="gameButton">
-     I scored {post.value} points for {post.description} in game {post.gameId} at {moment(post.createdAt).format("MMM Do YYYY, h:mm:ss a")} .
-       </p> 
-   </li>
+        <p className="gameButton">
+          I scored {post.value} points for {post.description} in game {post.gameId} at {moment(post.createdAt).format("MMM Do YYYY, h:mm:ss a")}.
+        </p> 
+        <p>{post.comment}</p>
+        <img width= '300px' src={post.image} alt=''></img>
+      </li>
 )
       
     return (
