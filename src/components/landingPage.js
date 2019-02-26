39d82dfd355c52fbe3b415ef118cacfb454ef36b
Sam Smiley <samtsmiley@gmail.com>
2019-02-26 09:10:01 -0800

    added spectator

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
+import {login} from '../actions/auth';
 import './landingPage.css';
 
 import LoginForm from './loginForm';
@@ -10,13 +11,20 @@ export function LandingPage(props) {
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
-
+    
+    const onSubmit = () =>  {
+        props.dispatch(login('spectator', '0123456789'));
+    }
+    
     return (
         <div className="home">
             <div className="landingPageLogo"></div>
             <h1>Welcome</h1>
             <p>Use this app to easily create or join games.</p>
             <p>Compete against other players to achieve Vicory!</p>
+            <p>Just want to spectate? 
+                <span className='link' onClick={()=>onSubmit()}> Click Here</span>
+            </p>
             <p>And Remember...</p>
             <h2>You Can't Win if You Don't Play</h2>
             <span>
