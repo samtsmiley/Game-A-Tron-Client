f3e9a9fcff3787d0582cc05262945c436988ed9c
seankrummel <sean.krummel@gmail.com>
2019-02-28 09:22:04 -0600

    added a redirect to the ConfirmEmail component

--- src/components/confirmEmail.js
+++ src/components/confirmEmail.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
 import {confirmEmail} from '../actions/email';
 
 export class ConfirmEmail extends React.Component {
+  state = {confirming: true};
   componentDidMount() {
     const id = this.props.match.params.id;
+    // console.log(id);
     this.props.dispatch(confirmEmail(id))
+    this.setState({confirming: false});
   }
 
   render() {
+    if (!this.state.confirming) return <Redirect to='/'/>
     return (
       <div className='confirmEmail'>
-        <p>{this.props.message}</p>
+        <p>Processing ... If you are not automatically redirected, please click <Link className='link' to='/'>this link</Link></p>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  message: state.email.message
+  message: state.email.message,
+  // error: state.email.error
 });
 export default connect(mapStateToProps)(ConfirmEmail);
