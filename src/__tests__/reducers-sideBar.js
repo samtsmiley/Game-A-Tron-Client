2d6aa0b75371c65dbf1349ea476a23b92a780b25
R-Cole <randy@goodglue.com>
2019-02-28 14:31:57 -0700

    testing sideBar actions-and-reducer

--- src/__tests__/reducers-sideBar.js
+++ src/__tests__/reducers-sideBar.js
@@ -1,5 +1,5 @@
 import sideBar from '../reducers/sideBar-reducer';
-import { fetchMyGamesRequest, showProfile, showAllPosts, showOneGame, showNewGame, showFindGame} from '../actions/sideBar-actions';
+import { fetchMyGamesRequest, fetchMyGamesSuccess, showProfile, showAllPosts, showOneGame, showNewGame, showFindGame} from '../actions/sideBar-actions';
 import rootReducer from '../reducers';
 
 describe('sideBar', () => {
@@ -23,45 +23,255 @@ describe('sideBar', () => {
         expect(sideBar(undefined,action)).toEqual(initialState);
          
     });
-
-    // it('Should return the current state on an unknown action', () => {
-    //     let currentState = {};
-    //     const state = sideBarReducer(currentState, {type: '__UNKNOWN'});
-    //     expect(state).toBe(currentState);
-    // });
-
-    // describe('addList', () => {
-    //     it('Should add new lists', () => {
-    //         let state;
-    //         state = sideBarReducer(state, addList(list1Title));
-    //         state = sideBarReducer(state, addList(list2Title));
-    //         expect(state).toEqual({
-    //             lists: [list1, list2]
-    //         });
-    //     });
-    // });
-
-    // describe('addCard', () => {
-    //     it('Should add new cards', () => {
-    //         let state = {
-    //             lists: [list1, list2]
-    //         };
-    //         state = sideBarReducer(state, addCard(card1Text, 0));
-    //         state = sideBarReducer(state, addCard(card2Text, 1));
-    //         state = sideBarReducer(state, addCard(card3Text, 1));
-    //         expect(state).toEqual({
-    //             lists: [
-    //                 {
-    //                     title: list1Title,
-    //                     cards: [card1]
-    //                 },
-    //                 {
-    //                     title: list2Title,
-    //                     cards: [card2, card3]
-    //                 }
-    //             ]
-    //         });
-    //     });
-    // });
 });
+
+describe('sideBar-SHOW_PROFILE', () => {
+
+    it('Should set showProfile key to boolean true and the other show... keys to boolean false', () => {
+
+        const newState = {
+            showProfile:true, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:[],
+            loadingComplete:false,
+            loading:false,
+            error:null
+            
+          };
+
+        const action = { type: 'SHOW_PROFILE'};
+
+        expect(sideBar(undefined,action)).toEqual(newState);
+  
+    });
+
+});
+ 
+describe('sideBar-SHOW_ALL_POSTS', () => {
+
+    it('Should set showAllPosts key to boolean true and the other show... keys to boolean false', () => {
+
+        const newState = {
+            showProfile:false, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:true,
+            myGames:[],
+            loadingComplete:false,
+            loading:false,
+            error:null
+            
+          };
+
+        const action = { type: 'SHOW_ALL_POSTS'};
+
+        expect(sideBar(undefined,action)).toEqual(newState);
+  
+    });
+
+});
+
+describe('sideBar-SHOW_ONE_GAME', () => {
+
+    it('Should set showOneGame key to boolean true and the other show... keys to boolean false', () => {
+
+        const newState = {
+            showProfile:false, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:true,
+            showAllPosts:false,
+            myGames:[],
+            loadingComplete:false,
+            loading:false,
+            error:null
+            
+          };
+
+        const action = { type: 'SHOW_ONE_GAME'};
+
+        expect(sideBar(undefined,action)).toEqual(newState);
+  
+    });
+
+});
+
+describe('sideBar-SHOW_NEW_GAME', () => {
+
+    it('Should set showNewGame key to boolean true and the other show... keys to boolean false', () => {
+
+        const newState = {
+            showProfile:false, 
+            showFindGame:false,
+            showNewGame:true,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:[],
+            loadingComplete:false,
+            loading:false,
+            error:null
+            
+          };
+
+        const action = { type: 'SHOW_NEW_GAME'};
+
+        expect(sideBar(undefined,action)).toEqual(newState);
+  
+    });
+
+});
+ 
+describe('sideBar-SHOW_FIND_GAME', () => {
+
+    it('Should set showFindGame key to boolean true and the other show... keys to boolean false', () => {
+
+        const newState = {
+            showProfile:false, 
+            showFindGame:true,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:[],
+            loadingComplete:false,
+            loading:false,
+            error:null
+            
+        };
+
+        const action = { type: 'SHOW_FIND_GAME'};
+
+        expect(sideBar(undefined,action))
+         .toEqual(newState);
+  
+    });
+
+});
+     
+describe('sideBar-FETCH_MYGAMES_REQUEST', () => {
+
+    it('Should set loading key to true and loadingComplete key to false and error key to null', () => {
+
+        const newState = {
+            showProfile:true, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:[], 
+            loadingComplete:false,
+            loading:true,
+            error:null
+            
+        };
+        
+        const action = { type: 'FETCH_MYGAMES_REQUEST'};
+
+        expect(sideBar(undefined,action))
+         .toEqual(newState); 
+
+    });
+
+});
+
+describe('sideBar-FETCH_MYGAMES_SUCCESS', () => {
+
+    it('Should add items to the myGames array and set loading key to false and loadingComplete key to true and error key to null', () => {
+
+        const newState = {
+            showProfile:true, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:['game1','game2','game3'], 
+            loadingComplete:true,
+            loading:false,
+            error:null
+            
+        };
+        
+        const action = { 
+                         type: 'FETCH_MYGAMES_SUCCESS',
+                         myGames: ['game1','game2','game3'],
+                         loadingComplete: true,
+                         loading: false,
+                         error: null
+                       };
+
+        expect(sideBar(undefined,action))
+         .toEqual(newState); 
+
+    });
+
+});
+
+describe('sideBar-FETCH_MYGAMES_ERROR', () => {
+
+    it('Should set error key to error', () => {
+
+        const newState = {
+            showProfile:true, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:[], 
+            loadingComplete:false,
+            loading:false,
+            error:'This all broke'
+            
+          };
+        
+          const action = { 
+                            type: 'FETCH_MYGAMES_ERROR',
+                            loading: false,
+                            error: 'This all broke'
+                          };
+
+
+        expect(sideBar(undefined,action))
+         .toEqual(newState); 
+
+    });
+
+});
+
+//version 2
+describe('sideBar-FETCH_MYGAMES_SUCCESS', () => {
+ 
+    it('Should add new items to myGames array -- version 2', () => {
+        const newState = {
+            showProfile:true, 
+            showFindGame:false,
+            showNewGame:false,
+            showOneGame:false,
+            showAllPosts:false,
+            myGames:['game1','game2','game3'], 
+            loadingComplete:true,
+            loading:false,
+            error:null
+            
+        };
+
+        let state;
+
+        const action = { 
+                         myGames: ['game1','game2','game3'],
+                         loadingComplete: true,
+                         loading: false,
+                         error: null
+                        };
+                
+        state = sideBar(state, fetchMyGamesSuccess(action.myGames));
+        expect(state).toEqual(newState); 
+                 
+   });
+
+});
+
+     
  
